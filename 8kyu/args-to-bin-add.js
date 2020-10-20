7b279d550c2c88917534d0207c056ce4fb3fa2f7
Damien Vassart <damienvassart@tutanota.com>
2020-10-20 17:56:12 +0200

    refactored solution (8kyu): Arguments to binary addition

--- 8kyu/args-to-bin-add.js
+++ 8kyu/args-to-bin-add.js
@@ -3,7 +3,4 @@ Given an array add all the number elements and return the binary equivalent of t
 to make the code bullet proof anything else than a number should be addeded as 0 since it can't be addeded.
 */
 
-function arr2bin(arr){
-  let filtered = arr.filter(e => typeof e === 'number');
-  return filtered.length > 0 ? filtered.reduce((a, b) => a + b).toString(2) : '0';
-}
+const arr2bin = (arr) => arr.reduce((acc, curr) => acc + (typeof curr === 'number' ? curr : 0), 0).toString(2);
