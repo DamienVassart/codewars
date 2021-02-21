dbb2a6d73a8a9e86c33bc803b06541d9fec9a1da
DamienVassart <damienvassart@tutanota.com>
2021-02-21 14:46:56 +0100

    added solution (7kyu): Sort by Last Char

---
+++ 7kyu/sort-by-last-char.js
@@ -0,0 +1,13 @@
+/*
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+*/
+
+function last(x){
+	let split = x.split(' ');
+	let last = [... new Set(split.map(str => str.slice(-1)).sort())];
+	return last.reduce((res, l) => res.concat(split.filter(str => str.slice(-1) === l)) , []);
+}
