cf0cb6d4ac278b883888840a49b7e68f2aa4c6d6
DamienVassart <damienvassart@tutanota.com>
2021-05-19 19:28:43 +0200

    added solution (C - 8kyu): Incorrect division method

---
+++ C/8kyu/divide_nums.c
@@ -0,0 +1,11 @@
+/*
+This method, 
+which is supposed to return the result of dividing its first argument by its second, 
+isn't always returning correct values. 
+Fix it.
+*/
+
+float divide_nums(int x, int y)
+{
+    return (float) x / y;
+}
